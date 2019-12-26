43feb5000c196f4b3e3efffa91c4cccd52fd7e67
Rudi Setiawan <rudestewing@gmail.com>
2019-12-26 17:40:18 +0700

    -

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -8,9 +8,22 @@ class CourseController extends Controller
 {
     public function index($folderName = '')
     {
-        $files = \Storage::disk('public')->files($folderName);
+        $files = (\Storage::disk('public')->files($folderName));
         $directories = \Storage::disk('public')->directories($folderName);
         
+        $files = count($files) ? collect($files)->map(function($item) {
+            return (object) [
+                'string' => $item,
+                'size' => $this->convertFilesize(\Storage::disk('public')->size($item), 2)
+            ];
+        }) : [];
+
+        $directories = count($directories) ? collect($directories)->map(function($item) {
+            return (object) [
+                'string' => $item,
+                'size' => $this->convertFilesize($this->countSizeInsideDir($item), 2),
+            ];
+        }) : [];
 
         $exploded = explode('/', $folderName);
         $paths = $this->generatePaths($exploded);
@@ -38,5 +51,20 @@ class CourseController extends Controller
         }
 
         return $paths;
+    }
+
+    private function countSizeInsideDir($dir)
+    {
+        $size = 0;
+        foreach(\Storage::disk('public')->allFiles($dir) as $item) {
+            $size += \Storage::disk('public')->size($item);
+        }
+        return $size;
+    }
+
+    private function convertFilesize($bytes, $decimals = 2){
+        $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) .' '.@$size[$factor];
     }
 }
--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -21,22 +21,25 @@
         <table class="table datatable-client-side">
             <thead>
                 <th> Name </th>
+                <th> Size </th>
                 <th> Type </th>
             </thead>
             @foreach ($directories as $dir)
                 <tr>
                     <td> 
-                        <a href="{{route('course.index', ['folderName' => $dir])}}"> <i class="far fa-folder"></i> {{$dir}} </a>    
-                        {{-- <a href="{{route('course.index', ['folderName' => $dir])}}"> <i class="far fa-folder"></i>  {{ $folderName != '' ? optional(explode($folderName.'/', $dir))[1] : $dir }} </a>     --}}
+                        <a href="{{route('course.index', ['folderName' => $dir->string])}}"> <i class="far fa-folder"></i> {{$dir->string}} </a>    
+                        {{-- <a href="{{route('course.index', ['folderName' => $dir->string])}}"> <i class="far fa-folder"></i>  {{ $folderName != '' ? optional(explode($folderName.'/', $dir->string))[1] : $dir->string }} </a>     --}}
                     </td>
+                    <td> {{$dir->size}} </td>
                     <td> Dir </td>
                 </tr>
             @endforeach
             @foreach ($files as $file)
                 <tr>
                     <td> 
-                        <a target="_blank" href="{{asset('storage/'.$file)}}"> <i class="fas fa-file"></i> {{ $folderName != '' ? optional(explode($folderName.'/', $file))[1] : $file }} </a>    
+                        <a target="_blank" href="{{asset('storage/'.$file->string)}}"> <i class="fas fa-file"></i> {{ $folderName != '' ? optional(explode($folderName.'/', $file->string))[1] : $file->string }} </a>    
                     </td>
+                    <td> {{$file->size}} </td>
                     <td> File </td>
                 </tr>
             @endforeach
